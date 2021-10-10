e0ea66b14ec86265344f503a463918daf2b9c8a0
samuilivanov23 <samuil2001ivanov@gmail.com>
2021-10-10 12:52:38 -0700

    (10-10-2021) Adding cars manager task

--- pik_java/exercises/off_tasks/cars_manager.java
+++ pik_java/exercises/off_tasks/cars_manager.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class StartUp
 {
 	public static void main( String args[] )
@@ -6,7 +8,7 @@ class StartUp
 	}
 }
 
-public class Car
+class Car
 {
 	private String brand;
 	private String model;
@@ -14,7 +16,7 @@ public class Car
 	private int horsePower;
 	private String engineType;
 	private String gearBoxType;
-	private Int manufactureYear;
+	private int manufactureYear;
 
 	public Car(){};
 
@@ -24,7 +26,7 @@ public class Car
 				int horsePower, 
 				String engineType, 
 				String gearBoxType, 
-				String manufactureYear)
+				int manufactureYear)
 	{
 		this.setBrand(brand);
 		this.setModel(model);
@@ -45,7 +47,7 @@ public class Car
 	public void setColor( String color ) { this.color = color; }
 
 	public int getHosrePower() { return this.horsePower; }
-	public void setHorsePower( String horsePower ) { this.horsePower = horsePower; }
+	public void setHorsePower( int horsePower ) { this.horsePower = horsePower; }
 
 	public String getEngineType() { return this.engineType; }
 	public void setEngineType( String engineType ) { this.engineType = engineType; }
@@ -53,7 +55,91 @@ public class Car
 	public String getGearBoxType() { return this.gearBoxType; }
 	public void setGearBoxType( String gearBoxType ) { this.gearBoxType = gearBoxType; }
 	
-	public String getManufactureYear() { return this.manufactureYear; }
-	public void setManufactureYear( String gearBoxType ) { this.manufactureYear = manufactureYear; }
+	public int getManufactureYear() { return this.manufactureYear; }
+	public void setManufactureYear( int manufactureYear ) { this.manufactureYear = manufactureYear; }
 
+	public Car[] FilterBrands(Car cars[], char brand_symbol)
+	{
+		Car filteredCars[] = new Car[cars.length];
+		int i = 0;
+		for( Car car: cars )
+		{
+			if( car.getBrand().charAt(0) == brand_symbol )
+			{
+				filteredCars[i] = car;
+				i++;
+			}
+		}
+
+		return filteredCars;
+	}
+
+	//0 -> ascending 1 -> descending 
+	public Car[] SortedCars( Car cars[], int sortingOrder )
+	{
+		for(int i = 0; i < ( cars.length - 1 ); i++)
+		{
+			for( int j = 0; j < cars.length; j++ )
+			{
+				if (sortingOrder == 0)
+				{
+					if( cars[i].brand.compareTo( cars[j].brand ) > 0 )
+					{
+						//Car tempCar = cars[i];
+						//cars[i] = cars[j];
+						//cars[j] = tempCar;
+						SwapCars( cars[i], cars[j] );
+					}
+				}
+				else
+				{
+					if( cars[i].brand.compareTo( cars[j].brand ) < 0 )
+					{
+						//Car tempCar = cars[i];
+						//cars[i] = cars[j];
+						//cars[j] = tempCar;
+						SwapCars( cars[i], cars[j] );
+					}
+				}
+				
+			}
+		}
+
+		return cars;
+	}
+
+	public Car[] RemoveCommonCars( Car cars[] )
+	{
+		Car resultCars[] = new Car[cars.length];
+		int count;
+		int index = 0;
+
+		for(int i = 0; i < ( cars.length - 1 ); i++)
+		{
+			count = 0;
+			for( int j = ( i + 1 ); j < cars.length; j++ )
+			{
+				if(cars[i] == cars[j])
+				{
+					count++;
+					break;
+				}	
+			}
+
+			if( count == 1 )
+			{
+				resultCars[index] = cars[i];
+				index++;
+			}
+		}
+
+		return resultCars;
+	}
+
+	private void SwapCars(Car car1, Car car2)
+	{
+		Car tempCar = car1;
+		car1 = car2;
+		car2 = tempCar;
+	}
 }
