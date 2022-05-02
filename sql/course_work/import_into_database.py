51283497cedcf133f138a46c8b1e3c72fae84187
samuilivanov23 <samuil2001ivanov@gmail.com>
2022-05-02 16:13:37 +0300

    (02-05-2022) Reformating import_into_database.py script

--- sql/course_work/import_into_database.py
+++ sql/course_work/import_into_database.py
@@ -13,29 +13,54 @@ def importData():
     session_type = ['зимна', 'лятна']
     group_number = ['41', '42', '43', '44', '45']
     course_number = ['1', '2', '3', '4']
-
-    for i in range(100, 300):
-        sql = 'insert into students (first_name, middle_name, last_name, faculty_number, group_number, course_number) values(%s, %s, %s, %s, %s, %s)'
-        cur.execute(sql, ("First_Name"+str(i), "Middle_Name"+str(i), "Last_Name"+str(i), "111111"+str(i), group_number[random.randint(0, 4)], course_number[random.randint(0, 3)]))
-   
-        sql = 'insert into subjects (name, introduction_date) values(%s, (SELECT cast((timestamp("2013-04-30 14:53:27") - INTERVAL FLOOR( RAND( ) * 730) DAY) as DATE)))'
-        cur.execute(sql, ("Name"+str(i),))
-
-        sql = 'insert into exams (name, conducting_date, session_type, average_score) values(%s, (SELECT timestamp("2013-04-30 14:53:27") - INTERVAL FLOOR( RAND( ) * 730) DAY), %s, %s)'
-        cur.execute(sql, ("Name"+str(i), session_type[random.randint(0, 1)], random.randint(1, 100)))
-    connection.commit()
-
-    for i in range(100, 300):
-        sql = 'insert into teachers (first_name, middle_name, last_name, subject_id) values(%s, %s, %s, %s)'
-        cur.execute(sql, ("First_Name"+str(i), "Middle_Name"+str(i), "Last_Name"+str(i), random.randint(1, 200)))    
-
-        sql = 'insert into students_exams (student_id, exam_id) values(%s, %s)'
-        cur.execute(sql, (random.randint(1, 200), random.randint(1, 200)))
-
-        sql = 'insert into students_subjects (student_id, subject_id) values(%s, %s)'
-        cur.execute(sql, (random.randint(1, 200), random.randint(1, 200)))
     
-    connection.commit()
+    try:
+        for i in range(100, 300):
+            #students
+            sql = ('INSERT INTO students (first_name, middle_name, last_name, faculty_number, group_number, course_number) ' 
+                    'VALUES(%s, %s, %s, %s, %s, %s)')
+            
+            cur.execute(sql, ("First_Name"+str(i), 
+                              "Middle_Name"+str(i), 
+                              "Last_Name"+str(i), 
+                              "111111"+str(i), 
+                              group_number[random.randint(0, 4)], 
+                              course_number[random.randint(0, 3)]))
+       
+            #subjects
+            sql = 'INSERT INTO subjects (name, introduction_date) ' 
+                  'VALUES(%s, (SELECT CAST((TIMESTAMP("2013-04-30 14:53:27") - INTERVAL FLOOR( RAND( ) * 730) DAY) as DATE)))'
+            
+            cur.execute(sql, ("Name"+str(i),))
+
+            #exams
+            sql = 'INSERT INTO exams (name, conducting_date, session_type, average_score) '
+                  'VALUES(%s, (SELECT TIMESTAMP("2013-04-30 14:53:27") - INTERVAL FLOOR( RAND( ) * 730) DAY), %s, %s)'
+            
+            cur.execute(sql, ("Name"+str(i), 
+                              session_type[random.randint(0, 1)], 
+                              random.randint(1, 100)))
+        connection.commit()
+
+        for i in range(100, 300):
+            #teachers
+            sql = 'INSERT INTO teachers (first_name, middle_name, last_name, subject_id) VALUES(%s, %s, %s, %s)'
+            
+            cur.execute(sql, ("First_Name"+str(i), "Middle_Name"+str(i), "Last_Name"+str(i), random.randint(1, 200)))    
+
+            #students_exams
+            sql = 'INSERT INTO students_exams (student_id, exam_id) VALUES(%s, %s)'
+
+            cur.execute(sql, (random.randint(1, 200), random.randint(1, 200)))
+
+            #students_subjects
+            sql = 'INSERT INTO students_subjects (student_id, subject_id) VALUES(%s, %s)'
+            
+            cur.execute(sql, (random.randint(1, 200), random.randint(1, 200)))
+        
+        connection.commit()
+    except Exception as e:
+        print(e)
 
     try:
         cur.close()
